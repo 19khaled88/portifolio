0da198bf04c5d5ba2761ee8e64487ed06e0a1036
19khaled88 <69922782+19khaled88@users.noreply.github.com>
2022-06-13 00:12:01 +0600

    navbar problem solve

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,6 +1,11 @@
+import { useState } from 'react'
 import { Link } from 'react-router-dom'
+import { Squash as Hamburger } from 'hamburger-react'
 import portfolio from '../assets/portfolio.png'
 const Navbar = () => {
+  const [isOpen, setOpen] = useState(false)
+
+  console.log(isOpen)
   return (
     <nav class="flex items-center justify-between flex-wrap bg-teal-500 p-4">
       <div class="flex items-center flex-shrink-0 text-white mr-6">
@@ -9,8 +14,22 @@ const Navbar = () => {
           Portfolio Website
         </span>
       </div>
+
       <div class="block lg:hidden">
-        <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
+        <Hamburger
+          color="#4FD1C5"
+          easing="ease-in"
+          onToggle={(toggled) => {
+            if (toggled) {
+              // open a menu
+              setOpen(!isOpen)
+            } else {
+              // close a menu
+              setOpen(!isOpen)
+            }
+          }}
+        />
+        {/* <button class="flex items-center px-3 py-2 border rounded text-teal-200 border-teal-400 hover:text-white hover:border-white">
           <svg
             class="fill-current h-3 w-3"
             viewBox="0 0 20 20"
@@ -19,49 +38,83 @@ const Navbar = () => {
             <title>Menu</title>
             <path d="M0 3h20v2H0V3zm0 6h20v2H0V9zm0 6h20v2H0v-2z" />
           </svg>
-        </button>
+        </button> */}
       </div>
-      <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
-        <div class="text-sm lg:flex-grow">
-          <Link
-            to="/resume"
-            href="#responsive-header"
-            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 font-bold text-green-600"
-          >
-            Resume
-          </Link>
-          <Link
-            to="/about"
-            href="#responsive-header"
-            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white  mr-4 font-bold text-green-600"
-          >
-            about me
-          </Link>
-          <Link
-            to="/blog"
-            href="#responsive-header"
-            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 font-bold  text-green-600"
-          >
-            Blog
-          </Link>
-          <Link
-            to="/contact"
-            href="#responsive-header"
-            class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white font-bold  text-green-600"
-          >
-            Contact
-          </Link>
+      {isOpen === true ? (
+        <div class="w-full block flex-grow lg:flex lg:items-center lg:w-auto">
+          <div class="text-sm lg:flex-grow ">
+            <Link
+              to="/resume"
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 font-bold text-green-600"
+            >
+              Resume
+            </Link>
+            <Link
+              to="/about"
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white  mr-4 font-bold text-green-600"
+            >
+              about me
+            </Link>
+            <Link
+              to="/blog"
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 font-bold  text-green-600"
+            >
+              Blog
+            </Link>
+            <Link
+              to="/contact"
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white font-bold  text-green-600"
+            >
+              Contact
+            </Link>
+          </div>
+          <div>
+            <Link
+              to="/"
+              class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0 font-bold"
+            >
+              Home
+            </Link>
+          </div>
         </div>
-        <div>
-          <Link
-            to="/"
-            href="#"
-            class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0 font-bold"
-          >
-            Home
-          </Link>
+      ) : (
+        <div class="w-full block flex-grow lg:flex lg:visible invisible h-1 lg:items-center lg:w-auto">
+          <div class="text-sm lg:flex-grow ">
+            <Link
+              to="/resume"
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 font-bold text-green-600"
+            >
+              Resume
+            </Link>
+            <Link
+              to="/about"
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white  mr-4 font-bold text-green-600"
+            >
+              about me
+            </Link>
+            <Link
+              to="/blog"
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white mr-4 font-bold  text-green-600"
+            >
+              Blog
+            </Link>
+            <Link
+              to="/contact"
+              class="block mt-4 lg:inline-block lg:mt-0 text-teal-200 hover:text-white font-bold  text-green-600"
+            >
+              Contact
+            </Link>
+          </div>
+          <div>
+            <Link
+              to="/"
+              class="inline-block text-sm px-4 py-2 leading-none border rounded text-white border-white hover:border-transparent hover:text-teal-500 hover:bg-white mt-4 lg:mt-0 font-bold"
+            >
+              Home
+            </Link>
+          </div>
         </div>
-      </div>
+      )}
     </nav>
   )
 }
