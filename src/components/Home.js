f2b9f6524b004751299c4f96200dff0efdc59bb9
19khaled88 <kkhaled88hasan@gmail.com>
2022-07-20 12:51:58 +0600

    remote react-particle-js

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,11 +1,5 @@
-import { Link } from 'react-router-dom'
-import { SocialMediaIconsReact } from 'social-media-icons-react'
-import user from '../assets/khaled_picture.jpg'
-import { useAnimationFrame } from 'framer-motion'
 import '../design/home.css'
 import Particle from './Particle'
-import { useRef } from 'react'
-import { refresh } from 'aos'
 const Home = () => {
   return (
     <div>
